2507fb7492072bb7ad32d14d2de0cc5e8ba95bbb
Kamil Szczygiel <distortec@distortec.com>
2019-04-25 12:18:48 +0200

    Add simple cmsis_os.h for distortos

---
+++ CMSIS-RTOS2-integration/include/cmsis_os.h
@@ -0,0 +1,20 @@
+/**
+ * \file
+ * \brief CMSIS-RTOS2 header for distortos
+ *
+ * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef CMSIS_RTOS2_INTEGRATION_INCLUDE_CMSIS_OS_H_
+#define CMSIS_RTOS2_INTEGRATION_INCLUDE_CMSIS_OS_H_
+
+/** API version (main[31:16].sub[15:0]) */
+#define osCMSIS 0x20001U
+
+#include "cmsis_os2.h"
+
+#endif	/* CMSIS_RTOS2_INTEGRATION_INCLUDE_CMSIS_OS_H_ */
