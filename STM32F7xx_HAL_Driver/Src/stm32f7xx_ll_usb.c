0af8b3f4de1480a5795e5356f6917a2e7cf3134a
Kamil Szczygiel <distortec@distortec.com>
2019-04-25 11:12:34 +0200

    Fix multiple errors in USB_HC_Halt()
    1. CHENA bit in USBx_HC(hcnum)->HCCHAR cannot be cleared - it may only
    be set to 1.
    
    2. Don't read and don't modify USBx_HC(hcnum)->HCCHAR multiple times, as
    this introduces race conditions.
    
    3. Implement proper channel halt procedure.

--- STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_usb.c
+++ STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_usb.c
@@ -1906,54 +1906,24 @@ HAL_StatusTypeDef USB_HC_Halt(USB_OTG_GlobalTypeDef *USBx, uint8_t hc_num)
 {
   uint32_t USBx_BASE = (uint32_t)USBx;
   uint32_t hcnum = (uint32_t)hc_num;
-  uint32_t count = 0U;
-  uint32_t HcEpType = (USBx_HC(hcnum)->HCCHAR & USB_OTG_HCCHAR_EPTYP) >> 18;
+  uint32_t hcchar = USBx_HC(hcnum)->HCCHAR;
+  uint32_t HcEpType = (hcchar & USB_OTG_HCCHAR_EPTYP) >> 18;
+
+  hcchar = (hcchar & ~USB_OTG_HCCHAR_CHENA) | USB_OTG_HCCHAR_CHDIS;
 
   /* Check for space in the request queue to issue the halt. */
   if ((HcEpType == HCCHAR_CTRL) || (HcEpType == HCCHAR_BULK))
   {
-    USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHDIS;
-
-    if ((USBx->HNPTXSTS & (0xFFU << 16)) == 0U)
-    {
-      USBx_HC(hcnum)->HCCHAR &= ~USB_OTG_HCCHAR_CHENA;
-      USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHENA;
-      do
-      {
-        if (++count > 1000U)
-        {
-          break;
-        }
-      }
-      while ((USBx_HC(hcnum)->HCCHAR & USB_OTG_HCCHAR_CHENA) == USB_OTG_HCCHAR_CHENA);
-    }
-    else
-    {
-      USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHENA;
-    }
+    if ((USBx->HNPTXSTS & (0xFFU << 16)) != 0U)
+      hcchar |= USB_OTG_HCCHAR_CHENA;
   }
   else
   {
-    USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHDIS;
-
-    if ((USBx_HOST->HPTXSTS & (0xFFU << 16)) == 0U)
-    {
-      USBx_HC(hcnum)->HCCHAR &= ~USB_OTG_HCCHAR_CHENA;
-      USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHENA;
-      do
-      {
-        if (++count > 1000U)
-        {
-          break;
-        }
-      }
-      while ((USBx_HC(hcnum)->HCCHAR & USB_OTG_HCCHAR_CHENA) == USB_OTG_HCCHAR_CHENA);
-    }
-    else
-    {
-      USBx_HC(hcnum)->HCCHAR |= USB_OTG_HCCHAR_CHENA;
-    }
+    if ((USBx_HOST->HPTXSTS & (0xFFU << 16)) != 0U)
+      hcchar |= USB_OTG_HCCHAR_CHENA;
   }
+
+  USBx_HC(hcnum)->HCCHAR = hcchar;
 
   return HAL_OK;
 }
